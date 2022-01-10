bb2e7f72cc4dff5c5e10c145b71353a1f263da00
kentozaka46 <kentoball52@gmail.com>
2022-01-11 07:40:58 +0900

    タスク1つのコンポーネント作成

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,33 @@
+import { Task } from "../type/Types";
+
+// コンポーネントに渡されるpropsの型
+type Props = {
+  task: Task;
+  handleDone: (task: Task) => void;
+  handleDelete: (task: Task) => void;
+};
+
+/**
+ * タスク1つを表すコンポーネント
+ * @author K.Kento
+ */
+const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) => {
+  return (
+    <li className={task.done ? "done" : ""}>
+      <label>
+        <input
+          className="checkbox-input"
+          type="checkbox"
+          onClick={() => handleDone(task)}
+          defaultChecked={task.done}
+        />
+        <span className="checkbox-label">{task.title}</span>
+      </label>
+      <button className="delete-btn" onClick={() => handleDelete(task)}>
+        削除
+      </button>
+    </li>
+  );
+};
+
+export default TaskItem;
