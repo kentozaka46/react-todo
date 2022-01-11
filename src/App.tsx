ce4bc078d27b940aea5c22f52dd4ea87718eb043
kentozaka46 <kentoball52@gmail.com>
2022-01-11 21:12:25 +0900

    Appコンポーネントの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import { Task } from "./type/Types";
 
-function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: "テスト",
+      done: false,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <div className="todo-component">
+        <TaskInput setTasks={setTasks} tasks={tasks} />
+        <TaskList setTasks={setTasks} tasks={tasks} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
