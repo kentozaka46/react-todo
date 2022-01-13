3a4660ff5b19014cdb91d1688fd3f6be55519192
kentozaka46 <kentoball52@gmail.com>
2022-01-13 20:16:43 +0900

    TODOリスト取得処理実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
+import { firebaseFirestore } from "./firebase";
 import { Task } from "./type/Types";
 
 const App: React.FC = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      id: 1,
-      title: "テスト",
-      done: false,
-    },
-  ]);
+  // TODOリストを格納するステート
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // firebaseに格納されているTODOリストを取得する処理
+  useEffect(() => {
+    const todos = collection(firebaseFirestore, "todos");
+    getDocs(todos).then((todo) =>
+      setTasks(
+        todo.docs.map((doc) => {
+          return {
+            id: doc.data().id,
+            title: doc.data().title,
+            done: doc.data().done,
+          };
+        })
+      )
+    );
+  }, []);
+
   return (
     <div className="todo-container">
       <div className="todo-component">
