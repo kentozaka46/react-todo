22c40add60483c637871f4f225024d902df92538
kentozaka46 <kentoball52@gmail.com>
2022-01-17 07:57:54 +0900

    ログイン画面製造

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import TaskView from "./components/TaskView";
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<TaskView />} />
         <Route path="signUp" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,49 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { firebaseAuth } from "../../firebase";
+
+const Login = () => {
+  // 入力されたメールアドレスを格納するステート
+  const [email, setEmail] = useState<string>("");
+  // 入力されたパスワードを格納するステート
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(firebaseAuth, email, password).then(
+      (userCredential) => {
+        const user = userCredential.user;
+      }
+    );
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={(event) => setEmail(event.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <button>ログイン</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
